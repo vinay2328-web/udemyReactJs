0553ac1358647b027433acd9920b4ed29bced5c0
= vinay <= vinaypathrikar2328@gmail.com>
2020-12-10 21:43:26 +0530

    Initial Commit

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import './Person.css';
+import styled from 'styled-components'
 
 const person = (props) => {
+
+    const StyledDiv = styled.div`
+        width: 60%;
+        margin: 16px auto;
+        padding: 10px;
+        border: 1px solid rgb(240, 236, 236);
+        box-shadow: 0px 2px 2px rgb(123, 121, 121);
+
+        @media(min-width: 500px) {
+            width: 450px;
+        }
+    `;
+
+    // const style = {
+    //     '@media(min-width: 500px)': {
+    //         width: '450px'
+    //     }
+    // }
+
+
     return (
         <>
-            <div className='Person'>
-                <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old!</p>
+            <StyledDiv>
+                <p onClick={props.click}>I'm a <b>{props.name} </b> and I am {props.age} years old!</p>
                 <p>{props.children}</p>
-            </div>
+                <input type="text" onChange={props.change} value={props.name} name={props.name} />
+            </StyledDiv>
         </>
     );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import ConditionalStf from './Component/StateFull/ConditionalSTF';
+// import ConditionalStl from './Component/StateLess/ConditionalSTL';
+// import Assignmenttwo from './AssignmentTwo/Assignmenttwo';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+// Component: APP
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// Component: StateFull conditional component
+ReactDOM.render(<ConditionalStf />, rootElement);
+
+// Component: StateFull conditional component
+// ReactDOM.render(<ConditionalStl />, rootElement);
+
+// Assignement Two: Class Component
+// ReactDOM.render(<Assignmenttwo />, rootElement);
